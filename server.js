3e6be679ec45bf675da4825bcf5f643a7164c215
Paul Shertzinger <pshertzi@gmail.com>
2022-02-28 17:39:45 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'dman1972',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
   });
 
 app.listen(PORT, () => {
-    console.log(`{server running on port ${PORT}`);
+    console.log(`server running on port ${PORT}`);
 });
