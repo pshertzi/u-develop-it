78a28155fab3a0658ceb458e073ee41e4ec158c6
Paul Shertzinger <pshertzi@gmail.com>
2022-03-02 15:07:58 -0500

    originized

---
+++ routes/apiRoutes/partyRoutes.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+// Get all parties
+router.get('/parties', (req, res) => {
+    const sql = 
+    `SELECT * FROM parties`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+// Get parties by id
+router.get('/party/:id', (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id=?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+  //delete parties 
+router.delete('/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+db.query(sql, params,(err, result) => {
+    if(err) {
+        res.status(400).json({error: err.message});
+        return;
+    } else if (!result.affectedRows) {
+        res.json({
+            message: 'Party not found'
+        });
+    } else {
+        res.json({
+            message: 'deleted',
+            changes: result.affectedRows,
+            id : req.params.id
+            });
+        }
+    });
+});
+
+module.exports = router;
