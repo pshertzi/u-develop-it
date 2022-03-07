bb8119e23a12b81bb42df6a2a5ea78ff16df01a5
Paul Shertzinger <pshertzi@gmail.com>
2022-03-07 12:28:56 -0500

    votes

--- routes/apiRoutes/candidateRoutes.js
+++ routes/apiRoutes/candidateRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { route } = require('.');
 const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
